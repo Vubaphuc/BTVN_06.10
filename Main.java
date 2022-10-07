28e9e4d9df997b57ab7e8323e370624ed4bf0efe
Vubaphuc <112857123+Vubaphuc@users.noreply.github.com>
2022-10-07 23:34:47 +0700

    BTVN_06.10_bài 1
    BTVN_06.10_bài 1

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // khai báo mảng 2 chiều + dữ liệu
+        String [][] bang = new String[][]{{"*","*","*","*"},{"*","*","*","*"},{"*","*","*","*"},{"*","*","*","*"}};
+        // in ra hình ta Vuông
+        for (int i = 0; i < bang.length; i++){
+            for (int j = 0; j < bang[0].length; j++){
+                // in ra hình * và khoảng cách
+                System.out.print(bang[i][j]+"  ");
+            }
+            //hết 1 hàng xuống dòng
+            System.out.println();
+        }
+
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+    public static void main(String[] args) {
+        // khai báo mảng 2 chiều + dữ liệu
+        String [][] bang = new String[][]{{" *","","",""},{" *","*","",""},{" *","*","*",""},{"*","*","*","*"}};
+        // in ra hình ta giác lệch
+        for (int i = 0; i < bang.length; i++){
+            for (int j = 0; j < bang[0].length; j++){
+                // in ra hình * và khoảng cách
+                System.out.print(bang[i][j]+"  ");
+            }
+            //hết 1 hàng xuống dòng
+            System.out.println();
+        }
+    }
+}
